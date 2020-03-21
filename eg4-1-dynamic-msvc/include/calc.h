5a31118f17eb849dad5b17c6d019c9e365868503
roy4801 <a82611141@gmail.com>
2020-03-22 05:30:41 +0800

    Add eg 4-1
    for shared(dynamic) library in MSVC

---
+++ eg4-1-dynamic-msvc/include/calc.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#if defined(_WIN32) || defined(__CYGWIN__)
+    #if defined(mycalc_EXPORTS)
+        #ifdef __GNUC__
+            #define MYCALC_API __attribute__ ((dllexport))
+        #else
+            #define MYCALC_API __declspec(dllexport)
+        #endif
+    #else
+        #ifdef __GNUC__
+            #define MYCALC_API __attribute__((dllimport))
+        #else
+            #define MYCALC_API __declspec(dllimport)
+        #endif
+    #endif
+#else
+    #if __GNUC__ >= 4
+        #define MYCALC_API __attribute__((visibility("default")))
+    #else
+        #define MYCALC_API
+    #endif
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+MYCALC_API int Cube(int x);
+MYCALC_API int Square(int x);
+
+#ifdef __cplusplus
+}
+#endif
